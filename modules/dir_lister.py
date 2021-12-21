01634f05c88b3b441379000e30848a943997fd45
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-21 14:15:28 -0500

    test modules

---
+++ modules/dir_lister.py
@@ -0,0 +1,6 @@
+import os
+
+def run(**args):
+    print('[*] In dir_lister module.')
+    files = os.listdir('.')
+    return str(files)
---
+++ modules/enviroment.py
@@ -0,0 +1,6 @@
+import os
+
+def run(**args):
+    print('[*] In enviroment module.')
+    return os.environ
+    
