3de720716fd1c49a871d178bb380ff0b6d35918e
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-23 16:51:12 -0500

    Update dir_lister.py

--- modules/dir_lister.py
+++ modules/dir_lister.py
@@ -2,6 +2,6 @@ import os
 
 def run(**args):
     print('[*] In dir_lister module.')
-    files = os.listdir('.')
-    print(str(files))
-    return str(files)
+#     files = os.listdir('.')
+#     print(str(files))
+#     return str(files)
