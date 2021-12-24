a869395a5ec0d3c1cce648926b54f44cf17a5bcb
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-23 19:48:17 -0500

    Update rookie.py

--- modules/rookie.py
+++ modules/rookie.py
@@ -1,5 +1,5 @@
 import json
 
 def run(**args):
-  basic_config = [{"module" : "dir_lister"},{"module" : "enviro"}]
+  basic_config =json.dumps([{"module" : "dir_lister"},{"module" : "enviro"}])
   return basic_config
