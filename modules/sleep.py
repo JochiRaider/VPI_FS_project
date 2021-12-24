10cd6b26819ddff53ef3de2dceae0460cfa20ae8
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-23 22:17:53 -0500

    Update sleep.py

--- modules/sleep.py
+++ modules/sleep.py
@@ -4,5 +4,7 @@ import random
 def run(**args):
    upper = 15
    lower = 10
-   print('[!] Sleep')
-   time.sleep(random.randint(lower, upper))
+   sleep_time = random.randint(lower, upper)
+   print('[!] Sleep:', sleep_time)
+   time.sleep(sleep_time)
+   return sleep_time
