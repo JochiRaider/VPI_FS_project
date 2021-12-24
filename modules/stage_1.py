145f91d3f9ee2561341352a0ecb88bc8c3e5e53f
JochiRaider <87738004+JochiRaider@users.noreply.github.com>
2021-12-23 22:05:24 -0500

    Update stage_1.py

--- modules/stage_1.py
+++ modules/stage_1.py
@@ -1,5 +1,6 @@
 import json
 
 def run(**args):
+  print('[$] Enter stage 1')
   basic_config =json.dumps([{"module" : "dir_lister"},{"module" : "enviro"},{"module" : "sleep"},{"module" : "stage_2"}])
   return basic_config
